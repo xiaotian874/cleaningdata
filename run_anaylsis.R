97d9ffdf04805f07d7d9cdc607eedbd3c582c113
Nicole <nicole.nan.zhang@gmail.com>
2017-09-03 22:24:24 -0400

    1
    1

---
+++ run_anaylsis.R
@@ -0,0 +1,49 @@
+if(!file.exists("./data")){dir.create("./data")}
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(fileUrl,destfile="./data/Dataset.zip")
+unzip(zipfile="./data/Dataset.zip",exdir="./data")
+
+x_train <- read.table("./data/UCI HAR Dataset/train/X_train.txt")
+y_train <- read.table("./data/UCI HAR Dataset/train/y_train.txt")
+subject_train <- read.table("./data/UCI HAR Dataset/train/subject_train.txt")
+
+x_test <- read.table("./data/UCI HAR Dataset/test/X_test.txt")
+y_test <- read.table("./data/UCI HAR Dataset/test/y_test.txt")
+subject_test <- read.table("./data/UCI HAR Dataset/test/subject_test.txt")
+
+
+features <- read.table('./data/UCI HAR Dataset/features.txt')
+
+activityLabels = read.table('./data/UCI HAR Dataset/activity_labels.txt')
+
+colnames(x_train) <- features[,2] 
+colnames(y_train) <-"activityId"
+colnames(subject_train) <- "subjectId"
+      
+colnames(x_test) <- features[,2] 
+colnames(y_test) <- "activityId"
+colnames(subject_test) <- "subjectId"
+      
+colnames(activityLabels) <- c(¡®activityId¡¯,¡®activityType')
+
+mrg_train <- cbind(y_train, subject_train, x_train)
+mrg_test <- cbind(y_test, subject_test, x_test)
+setAllInOne <- rbind(mrg_train, mrg_test)
+
+colNames <- colnames(setAllInOne)
+mean_and_std <- (grepl("activityId" , colNames) | 
+                 grepl("subjectId" , colNames) | 
+                 grepl("mean.." , colNames) | 
+                 grepl("std.." , colNames) 
+                 )
+
+setForMeanAndStd <- setAllInOne[ , mean_and_std == TRUE]
+
+setWithActivityNames <- merge(setForMeanAndStd, activityLabels,
+                              by='activityId',
+                              all.x=TRUE)
+
+Tidy <- aggregate(. ~subjectId + activityId, setWithActivityNames, mean)
+Tidy <- Tidy[order(Tidy$subjectId, Tidy$activityId),]
+
+write.table(Tidy, "output.txt", row.name=FALSE)
